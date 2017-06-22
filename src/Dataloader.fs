9d843453218ba23777f1f7e278a3e145cc0ecebc
Reed Mullanix <reed.mullanix@mybazinga.com>
2017-06-22 15:59:59 -0700

    Initial commit

---
+++ src/Dataloader.fs
@@ -0,0 +1,172 @@
+module FSharp.Data.Dataloader
+
+open System
+open System.Reflection
+/// Represents an instruction to the system on how to fetch a value of type 'a
+
+/// Type encapsulating the delayed resolution of a Result
+type Fetch<'a> = { unFetch : DataCache ref -> RequestStore ref -> Result<'a> }
+
+/// Type representing the result of a data fetch
+/// Done represents a completed fetch with value of type 'a
+and Result<'a> =
+    | Done of 'a
+    | Blocked of BlockedRequest list * Fetch<'a>
+    | FailedWith of exn
+
+/// Type representing the status of a blocked request
+and FetchStatus<'a> =
+    | NotFetched
+    | FetchSuccess of 'a
+    | FetchError of exn
+
+
+/// Represents an untyped Request, used to power the caching side
+and Request = 
+    abstract Identifier : string
+
+and DataSource = 
+    abstract Name : string
+    abstract FetchFn : (BlockedRequest list -> unit)
+
+and DataSource<'r when 'r :> Request> =
+    abstract FetchFn : BlockedFetch<'r> list -> unit
+    abstract Name : string
+
+and BlockedFetch<'r> = {
+    Request: 'r
+    Status: FetchStatus<obj> ref
+}
+and BlockedRequest = {
+    Request: obj
+    Status: FetchStatus<obj> ref
+}
+
+and DataCache = Cache of Map<string, FetchStatus<obj> ref>
+
+
+/// When a request is issued by the client via a 'dataFetch',
+/// It is placed in the RequestStore. When we are ready to fetch a batch of requests,
+/// TODO
+and RequestStore = Store of Map<string, DataSource * BlockedRequest list>
+
+
+[<RequireQualifiedAccess>]
+module internal DataCache =
+    let empty () = Cache(Map.empty)
+    let add (r: Request) (status: FetchStatus<obj> ref) (c: DataCache) =
+        let (Cache c') = c
+        Cache(Map.add r.Identifier status c')
+    
+    let get (r: Request) (c: DataCache) =
+        let (Cache c') = c
+        Map.tryFind r.Identifier c'
+
+[<RequireQualifiedAccess>]
+module internal RequestStore =
+    let empty () = Store(Map.empty)
+    let addRequest (r: BlockedRequest) (source: DataSource) (store: RequestStore) =
+        let (Store map) = store
+        let name = source.Name
+        match Map.tryFind name map with
+        | Some (_ ,requests) -> Store(Map.add name (source, r::requests) map)
+        | None -> Store(Map.add name (source, [r]) map)
+    
+    let resolve (fn: DataSource -> BlockedRequest list -> unit) (store: RequestStore) =
+        let (Store map) = store
+        Map.iter (fun _ (s, b) -> fn s b) map
+
+
+[<RequireQualifiedAccess>]
+module Fetch =
+
+    /// Applies a mapping function f to the inner value of a
+    let rec map f a =
+        let unFetch = fun cacheRef storeRef ->
+            match a.unFetch cacheRef storeRef with
+            | Done x -> Done(f x)
+            | Blocked (br, c) -> Blocked(br, (map f c))
+            | FailedWith exn -> FailedWith exn
+        { unFetch = unFetch }
+
+    /// Applies some wrapped function f to the inner value of a
+    let rec apply f a =
+        let unFetch = fun cacheRef storeRef ->
+            match f.unFetch cacheRef storeRef, a.unFetch cacheRef storeRef with
+            | Done f', Done a' -> Done(f' a')
+            | Done f', Blocked(br, a') -> Blocked(br, map f' a')
+            | Blocked(br, f'), Done a' -> Blocked(br, apply f' { unFetch = fun cacheRef storeRef -> Done a' })
+            | Blocked(br1, f'), Blocked(br2, a') -> Blocked(br1@br2, apply f' a')
+            | FailedWith exn, _ -> FailedWith exn
+            | _, FailedWith exn -> FailedWith exn
+        { unFetch = unFetch }
+    
+    /// Applies some binding function f to the inner value of a
+    let rec bind f a =
+        let unFetch = fun cacheRef storeRef ->
+            match a.unFetch cacheRef storeRef with
+            | Done x -> (f x).unFetch cacheRef storeRef
+            | Blocked(br, x) -> Blocked(br, bind f x)
+            | FailedWith exn -> FailedWith exn
+        { unFetch = unFetch }
+    
+    /// Transforms a request into a fetch operation
+    let dataFetch<'a, 'r when 'r :> Request> (d: DataSource<'r>) (a: Request): Fetch<'a> =
+        let cont (statusRef: FetchStatus<obj> ref) = 
+            let unFetch cacheRef storeRef = 
+                let status = !statusRef 
+                match status with
+                | FetchSuccess s -> Done(s :?> 'a)
+                | _ -> FailedWith (Failure "Expected Complete Fetch!")
+            { unFetch = unFetch }
+        let unFetch cacheRef storeRef =
+            let cache = !cacheRef
+            // Do a lookup in the cache to see if we need to return 
+            match DataCache.get a cache with
+            | Some statusRef ->
+                match !statusRef with
+                | FetchSuccess (v:obj) ->
+                    // We've seen the request before, and it is completed, so return the value
+                    Done(v :?> 'a)
+                | NotFetched ->
+                    // We've seen the request before, but it is blocked, but we dont add the request to the RequestStore
+                    Blocked([], cont statusRef)
+                | FetchError ex ->
+                    // There was an error, so add the failure as our result
+                    FailedWith ex
+            | None -> 
+                // We haven't seen the request before, so add it to the request store so it will be fetched in the next round
+                let statusRef = ref NotFetched
+                let blockedReq = {Request = box a; Status = statusRef}
+                // Update the cache and store references
+                let datasource = 
+                    { new DataSource with 
+                        member x.Name = d.Name
+                        member x.FetchFn = List.map(fun b -> { BlockedFetch.Request = b.Request :?> 'r; Status = b.Status}) >> d.FetchFn
+                    }
+                cacheRef := DataCache.add a statusRef cache
+                storeRef := RequestStore.addRequest blockedReq datasource !storeRef
+                Blocked([blockedReq], cont statusRef)
+        { unFetch = unFetch }
+    
+    /// Issues a batch of fetches to the request store. After 
+    /// 'performFetchs' is complete, all of the BlockedRequests status refs are full
+    let performFetches (store: RequestStore) =
+        RequestStore.resolve(fun source blocked -> source.FetchFn blocked) store
+
+
+
+    /// Executes a fetch using fn to resolve the blocked requests
+    /// Fn should fill in the reference value of the BlockedRequest
+    let runFetch fetch =
+        let storeRef = ref (RequestStore.empty ()) 
+        let cacheRef = ref (DataCache.empty ())
+        let rec helper f =
+            match fetch.unFetch cacheRef storeRef with
+            | Done a -> a
+            | Blocked(br, cont) ->
+                performFetches (!storeRef)
+                helper cont
+            | FailedWith ex -> raise ex
+        helper fetch
+
---
+++ tests/DataloaderTests.fs
@@ -0,0 +1,72 @@
+module Tests
+open System
+open FSharp.Data.Dataloader
+
+
+// === An API ===
+
+type PostId = int
+type PostContent = string
+type Enviroment = {
+    PostIds : PostId list
+    PostContents : Map<PostId, PostContent>
+}
+
+let env = {
+    PostIds = [1;2]
+    PostContents = Map.ofList [1, "Test1"; 2, "Test2"]
+}
+
+// The original HAXL library relies HEAVYILY on GADTS for request types
+// This presents some problems for F#, as we don't have that great of a way of 
+// expressing that.
+// type FetchPostIdRequest =
+//     | FetchPosts //of Blog<Enviroment, PostId list>
+//     interface Request<PostId list> with
+//         member x.Identifier = 
+//             match x with
+//             | FetchPosts _ -> "PostIdRequest"
+
+// type FetchPostContentRequest = 
+//     | FetchPostContent of PostId
+//     interface Request<PostContent option> with
+//         member x.Identifier = 
+//             match x with
+//             | FetchPostContent id -> "PostContent id = " + id.ToString()
+
+type BlogRequest =
+    | FetchPosts
+    | FetchPostContent of PostId
+    interface Request with
+        member x.Identifier =
+            match x with
+            | FetchPosts -> "FetchPosts"
+            | FetchPostContent id -> "FetchContent id = " + id.ToString()
+        
+
+type BlogDataSource() = 
+    interface DataSource<BlogRequest> with
+        member x.Name = "Blog"
+        member x.FetchFn (blocked: BlockedFetch<BlogRequest> list) = 
+            blocked
+            |> List.iter(fun b ->
+                match b.Request with
+                | FetchPosts -> 
+                    let ids = env.PostIds
+                    b.Status := FetchSuccess(box ids)
+                | FetchPostContent id ->
+                    let content = Map.tryFind id env.PostContents
+                    b.Status := FetchSuccess(box content))
+
+[<EntryPoint>]
+let main args = 
+    printfn "Starting Fetch!"
+    let source = BlogDataSource()
+    let fetchPostIds = Fetch.dataFetch<PostId list, BlogRequest> source (FetchPosts)
+    let fetchPostContent id = Fetch.dataFetch<PostContent option, BlogRequest> source (FetchPostContent id)
+    let contents =
+        fetchPostIds
+        |> Fetch.bind((List.head >> fetchPostContent))
+    let res =  Fetch.runFetch contents
+    printfn "Results: %A" res
+    0
